98fe488c890d0c39ddb279a5847aaecbd81ca657
BernardoBarc <167653729+BernardoBarc@users.noreply.github.com>
2025-11-20 15:51:14 -0300

    Update page.tsx

--- FrontEnd/espaco-marias/src/app/Painel/manicures/dados/page.tsx
+++ FrontEnd/espaco-marias/src/app/Painel/manicures/dados/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
