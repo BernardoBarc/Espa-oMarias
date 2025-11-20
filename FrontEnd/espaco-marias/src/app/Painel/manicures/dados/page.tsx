2f68600e05896dc9697a56a7f8e890c580283306
BernardoBarc <167653729+BernardoBarc@users.noreply.github.com>
2025-11-20 16:15:22 -0300

    Update page.tsx

--- FrontEnd/espaco-marias/src/app/Painel/manicures/dados/page.tsx
+++ FrontEnd/espaco-marias/src/app/Painel/manicures/dados/page.tsx
@@ -2,7 +2,6 @@ export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
 import React, { Suspense } from "react";
-import EditarManicuresClient from "./EditarManicuresClient";
 
 export default function Page() {
 return (
