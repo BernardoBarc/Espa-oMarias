ff7122a103b33d823b7a01fbe4cadb95253aa81f
BernardoBarc <167653729+BernardoBarc@users.noreply.github.com>
2025-11-20 20:09:59 -0300

    Update page.tsx

--- FrontEnd/espaco-marias/src/app/Painel/manicures/dados/page.tsx
+++ FrontEnd/espaco-marias/src/app/Painel/manicures/dados/page.tsx
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 import React, { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-import ChangePasswordModal from "FrontEnd/espaco-marias/src/components/ChangePasswordModal.tsx";
+import ChangePasswordModal from "../../../../components/ChangePasswordModal";
 
 export default function Page() {
 return (
