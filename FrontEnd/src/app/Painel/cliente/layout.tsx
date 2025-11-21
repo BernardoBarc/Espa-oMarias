f9bdc06552ff167831f622ec8a5ea78790d65c6f
BernardoBarc <bbarc67@gmail.com>
2025-11-20 21:04:12 -0300

    commit 1

--- FrontEnd/src/app/Painel/cliente/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ClienteLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-[#232526] to-[#414345] flex flex-col">
-      {children}
-    </div>
-  );
-}
