3d87c54bde9580239a999d0e15e3e57d0267e069
BernardoBarc <bbarc67@gmail.com>
2025-12-04 21:34:49 -0300

    Atualização emailService.js

--- BackEnd/services/emailService.js
+++ BackEnd/services/emailService.js
@@ -256,7 +256,6 @@ export const sendAppointmentNotification = async (email, appointmentDetails) => 
   return result;
 };
 
-//exportar funções
 export default { 
   sendEmail, 
   sendEmailVerificationCode, 
